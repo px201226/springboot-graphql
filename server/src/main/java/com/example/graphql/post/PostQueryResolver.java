109c4267a7181deb071017f6618af4422c8c5f3a
DESKTOP-T0VS20O\Lee-Gi-Su <px201226@gmail.com>
2021-06-28 15:08:31 +0900

    Post 리소스 JPA N+1문제 Critera API로 해결

--- server/src/main/java/com/example/graphql/post/PostQueryResolver.java
+++ server/src/main/java/com/example/graphql/post/PostQueryResolver.java
@@ -4,10 +4,15 @@ import com.example.graphql.post.Post;
 import com.example.graphql.post.PostRepository;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.DataFetchingFieldSelectionSet;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.criteria.Fetch;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import java.util.List;
 
 
@@ -19,6 +24,25 @@ public class PostQueryResolver implements GraphQLQueryResolver {
 
     @PreAuthorize("permitAll")
     public List<Post> posts(DataFetchingEnvironment environment){
-        return postRepository.findAll();
+        DataFetchingFieldSelectionSet selectionSet = environment.getSelectionSet();
+        if(selectionSet.contains("comments")){
+            return postRepository.findAll(fetchComments());
+        }else{
+            return postRepository.findAll();
+        }
+    }
+
+    @PreAuthorize("permitAll")
+    public Post findByPostId(Long postId){
+        return postRepository.findById(postId).orElseThrow(RuntimeException::new);
+    }
+
+
+    private Specification fetchComments(){
+        return ((root, query, criteriaBuilder) -> {
+            Fetch f = root.fetch("comments", JoinType.LEFT);
+            Join join = (Join) f;
+            return join.getOn();
+        });
     }
 }
